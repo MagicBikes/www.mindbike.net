c3b5772d87a0ee7a2f32448854f1058843ff2992
David Lynn Skinner <skinner.david@gmail.com>
2025-06-23 08:39:10 -0500

    add docs

---
+++ docs/searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "MindBike is a new brainer",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "This is your new home page.",
+    "description": "This is your new home page.",
+    "tags": [],
+    "title": "MindBike is a new brainer",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "MindBike is a new brainer",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module www.mindbike.net
 
 go 1.23.4
+
+require github.com/McShelby/hugo-theme-relearn v0.0.0-20250603165220-e59b0bd36c54 // indirect
